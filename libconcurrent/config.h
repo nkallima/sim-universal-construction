0059e10278fd9838c587c7c0e2c1a9fb1b5e3189
Nikolaos D. Kallimanis <nkallima@gmail.com>
2020-12-20 18:05:08 +0200

    This patch automatic detects the appropriate set of libraries (i.e., papi, numa, etc.) needed by the config.h and sets the appropriate compiler arguments (i.e., -lpapi, -lnuma) while building the sources.

--- libconcurrent/config.h
+++ libconcurrent/config.h
@@ -71,7 +71,6 @@
 // By enabling this definition, the Performance Application Programming Interface (PAPI)
 // is used for getting performance counters during the execution of benchmarks.
 // In this case, the PAPI library should be install and appropriately configured.
-// IF ENABLE, DO NOT FORGET TO ADD -lpapi OPTION in LDLIBS VARIABLE IN Makefile.generic!
 //#define _TRACK_CPU_COUNTERS
 
 //#define _EMULATE_FAA_
