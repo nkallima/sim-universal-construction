b72af5593664b9249654c54bb9a263dcf020bdb9
Nikolaos D. Kallimanis <nkallima@gmail.com>
2021-06-02 22:27:41 +0300

    Adding documentation for the API of ccqueue.h. Improvements on the documentation of ccsynch.h.

--- libconcurrent/includes/ccqueue.h
+++ libconcurrent/includes/ccqueue.h
@@ -1,3 +1,13 @@
+/// @file ccqueue.h
+/// @author Nikolaos D. Kallimanis (nkallima@gmail.com)
+/// @brief This file exposes the API of the CC-Queue concurrent queue implementation.
+/// An example of use of this API is provided in benchmarks/ccqueuebench.c file.
+///
+/// For a more detailed description see the original publication:
+/// Panagiota Fatourou, and Nikolaos D. Kallimanis."Revisiting the combining synchronization technique".
+/// ACM SIGPLAN Notices. Vol. 47. No. 8. ACM, PPoPP 2012.
+/// @copyright Copyright (c) 2021
+
 #ifndef _CCSTACK_H_
 #define _CCSTACK_H_
 
@@ -5,22 +15,61 @@
 #include <queue-stack.h>
 #include <ccsynch.h>
 
+/// @brief CCQueueStruct stores the state of an instance of the CC-Queue concurrent queue implementation.
+/// CCQueueStruct should be initialized using the CCQueueStructInit function.
 typedef struct CCQueueStruct {
+    /// @brief A CC-Synch instance for servicing the enqueue operations.
     CCSynchStruct enqueue_struct CACHE_ALIGN;
+    /// @brief A CC-Synch instance for servicing the dequeue operations.
     CCSynchStruct dequeue_struct CACHE_ALIGN;
+    /// @brief A pointer to the last inserted element.
     volatile Node *last CACHE_ALIGN;
+    /// @brief A pointer to the first inserted element.
     volatile Node *first CACHE_ALIGN;
+    /// @brief A guard node that it is used only at the initialization of the queue.
     Node guard CACHE_ALIGN;
 } CCQueueStruct;
 
+/// @brief CCQueueThreadState stores each thread's local state for a single instance of CC-Queue.
+/// For each instance of CC-Queue, a discrete instance of CCQueueThreadState should be used.
 typedef struct CCQueueThreadState {
+    /// @brief A CCSynchThreadState for the instance of CC-Synch that serves the enqueue operations.
     CCSynchThreadState enqueue_thread_state;
+    /// @brief A CCSynchThreadState for the instance of CC-Synch that serves the dequeue operations.
     CCSynchThreadState dequeue_thread_state;
 } CCQueueThreadState;
 
+/// @brief This function initializes an instance of the CC-Queue concurrent queue implementation.
+///
+/// This function should be called once (by a single thread) before any other thread tries to
+/// apply any enqueue or dequeue operation.
+///
+/// @param queue_object_struct A pointer to an instance of the CC-Queue concurrent queue implementation.
+/// @param nthreads The number of threads that will use the CC-Queue concurrent queue implementation.
 void CCQueueStructInit(CCQueueStruct *queue_object_struct, uint32_t nthreads);
+
+/// @brief This function should be called once before the thread applies any operation to the CC-Queue concurrent queue implementation.
+///
+/// @param object_struct A pointer to an instance of the CC-Queue concurrent queue implementation.
+/// @param lobject_struct A pointer to thread's local state of CC-Queue.
+/// @param pid The pid of the calling thread.
 void CCQueueThreadStateInit(CCQueueStruct *object_struct, CCQueueThreadState *lobject_struct, int pid);
+
+/// @brief This function adds (i.e. enqueues) a new element to the back of the queue.
+/// This element has a value equal with arg.
+///
+/// @param object_struct A pointer to an instance of the CC-Queue concurrent queue implementation.
+/// @param lobject_struct A pointer to thread's local state of CC-Queue.
+/// @param arg The enqueue operation will insert a new element to the queue with value equal to arg.
+/// @param pid The pid of the calling thread.
 void CCQueueApplyEnqueue(CCQueueStruct *object_struct, CCQueueThreadState *lobject_struct, ArgVal arg, int pid);
+
+/// @brief This function removes (i.e. dequeues) an element from the from of queue and returns its value.
+///
+/// @param object_struct A pointer to an instance of the CC-Queue concurrent queue implementation.
+/// @param lobject_struct A pointer to thread's local state of CC-Queue.
+/// @param pid The pid of the calling thread.
+/// @return The value of the removed element.
 RetVal CCQueueApplyDequeue(CCQueueStruct *object_struct, CCQueueThreadState *lobject_struct, int pid);
 
 #endif
--- libconcurrent/includes/ccsynch.h
+++ libconcurrent/includes/ccsynch.h
@@ -1,6 +1,6 @@
 /// @file ccsynch.h
 /// @author Nikolaos D. Kallimanis (nkallima@gmail.com)
-/// @brief This file exposes the API of the CCSynch combining object.
+/// @brief This file exposes the API of the CC-Synch combining object.
 /// An example of use of this API is provided in benchmarks/ccsynchbench.c file.
 ///
 /// For a more detailed description see the original publication:
@@ -39,23 +39,23 @@ typedef struct CCSynchNode {
     char align[PAD_CACHE(sizeof(HalfCCSynchNode))];
 } CCSynchNode;
 
-/// @brief CCSynchThreadState stores each thread's local state for a single instance of CCSynch.
-/// For each instance of CCSynch, a discrete instance of CCSynchThreadState should be used.
+/// @brief CCSynchThreadState stores each thread's local state for a single instance of CC-Synch.
+/// For each instance of CC-Synch, a discrete instance of CCSynchThreadState should be used.
 typedef struct CCSynchThreadState {
     /// @brief pointer to an empty request that would be used for announcing future requests.
     CCSynchNode *next;
-    /// @brief A toggle-bit used by the CCSynch object.
+    /// @brief A toggle-bit used by the CC-Synch object.
     int toggle;
 } CCSynchThreadState;
 
-/// @brief CCSynchStruct stores the state of an instance of the a CCSynch combining object.
+/// @brief CCSynchStruct stores the state of an instance of the a CC-Synch combining object.
 /// CCSynchStruct should be initialized using the CCSynchStructInit function.
 typedef struct CCSynchStruct {
     /// @brief Tail points to the most recently announced request. Initially, it points to NULL.
     volatile CCSynchNode *Tail CACHE_ALIGN;
     /// @brief Pointer to the pool of nodes used by threads in order to announce their requests.
     CCSynchNode *nodes CACHE_ALIGN;
-    /// @brief The number of threads that will use the CCSynch combining object.
+    /// @brief The number of threads that will use the CC-Synch combining object.
     uint32_t nthreads;
 #ifdef DEBUG
     volatile uint64_t counter CACHE_ALIGN;
@@ -64,27 +64,27 @@ typedef struct CCSynchStruct {
 } CCSynchStruct;
 
 
-/// @brief This function initializes an instance of the CCSynch combining object.
+/// @brief This function initializes an instance of the CC-Synch combining object.
 ///
 /// This function should be called once (by a single thread) before any other thread tries to
 /// apply any request by using the CCSynchApplyOp function.
 ///
-/// @param l A pointer to an instance of the CCSynch combining object.
-/// @param nthreads The number of threads that will use the CCSynch combining object.
+/// @param l A pointer to an instance of the CC-Synch combining object.
+/// @param nthreads The number of threads that will use the CC-Synch combining object.
 void CCSynchStructInit(CCSynchStruct *l, uint32_t nthreads);
 
-/// @brief This function should be called once before the thread applies any operation to the CCSynch combining object.
+/// @brief This function should be called once before the thread applies any operation to the CC-Synch combining object.
 ///
-/// @param l A pointer to an instance of the CCSynch combining object.
-/// @param st_thread A pointer to thread's local state of CCSynch.
+/// @param l A pointer to an instance of the CC-Synch combining object.
+/// @param st_thread A pointer to thread's local state of CC-Synch.
 /// @param pid The pid of the calling thread.
 void CCSynchThreadStateInit(CCSynchStruct *l, CCSynchThreadState *st_thread, int pid);
 
 /// @brief This function is called whenever a thread wants to apply an operation to the simulated concurrent object.
 ///
-/// @param l A pointer to an instance of the CCSynch combining object.
-/// @param st_thread A pointer to thread's local state for a specific instance of CCSynch.
-/// @param sfunc A serial function that the CCSynch instance should execute, while applying requests announced by active threads.
+/// @param l A pointer to an instance of the CC-Synch combining object.
+/// @param st_thread A pointer to thread's local state for a specific instance of CC-Synch.
+/// @param sfunc A serial function that the CC-Synch instance should execute, while applying requests announced by active threads.
 /// @param state A pointer to the state of the simulated object.
 /// @param arg The argument of the request that the thread wants to apply.
 /// @param pid The pid of the calling thread.
