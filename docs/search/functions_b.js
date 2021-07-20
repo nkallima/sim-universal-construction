0f3ff5105c4564267b876a0f5173474c7e74ff9c
Nikolaos D. Kallimanis <nkallima@gmail.com>
2021-07-20 14:28:38 +0300

    Documentation upload.

--- docs/search/functions_b.js
+++ docs/search/functions_b.js
@@ -4,8 +4,8 @@ var searchData=
   ['mcslockinit_614',['MCSLockInit',['../mcs_8h.html#a01cf4b56395b1a1c9e754d152b632ce2',1,'mcs.h']]],
   ['mcsthreadstateinit_615',['MCSThreadStateInit',['../mcs_8h.html#abe18f92cdd12bbd0087aecf91bec6c91',1,'mcs.h']]],
   ['mcsunlock_616',['MCSUnlock',['../mcs_8h.html#a89042866e56fb10ae4cb07b96a3677a4',1,'mcs.h']]],
-  ['msqueuedequeue_617',['MSQueueDequeue',['../msqueue_8h.html#abfe3b5d5f98ba27b83fe34f29a3d8f7a',1,'msqueue.h']]],
-  ['msqueueenqueue_618',['MSQueueEnqueue',['../msqueue_8h.html#a224d13d6459fae65e82019685f8e2d13',1,'msqueue.h']]],
-  ['msqueueinit_619',['MSQueueInit',['../msqueue_8h.html#a31851c4e8a9d4cd2cb5a347262a7274e',1,'msqueue.h']]],
+  ['msqueuedequeue_617',['MSQueueDequeue',['../msqueue_8h.html#a09fb47175d4222f798ed1b53a9a87b0a',1,'msqueue.h']]],
+  ['msqueueenqueue_618',['MSQueueEnqueue',['../msqueue_8h.html#ad846820701962b0d3f65b17d90874390',1,'msqueue.h']]],
+  ['msqueueinit_619',['MSQueueInit',['../msqueue_8h.html#aa7b69d2da361672fe922f44c51c4f8f4',1,'msqueue.h']]],
   ['msqueuethreadstateinit_620',['MSQueueThreadStateInit',['../msqueue_8h.html#a2c366ca204d2ea2ef6034984de3d574a',1,'msqueue.h']]]
 ];
